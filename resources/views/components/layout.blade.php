8416c85249ed2ada35f952e0e6379fffca38ed75
mrmatteorusso <mrmatteorusso:gmail.com>
2024-10-18 11:45:12 +0200

    edit operation2

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -17,30 +17,6 @@
 
 <body class="bg-white">
 
-    <div id="editor">
-
-        <div class="card bg-base-100  shadow-xl flex-none mb-8 mt-8 w-1/2 mx-auto">
-            <div class="card-body">
-                <form action="/events" class="space-y-4" method="POST">
-                    @csrf
-
-
-                    <!-- Event Description -->
-                    <div class="form-control">
-                        <label for="editor" class="label">
-                            <span class="label-text">Event Description</span>
-                        </label>
-                        <textarea id="editor1" name="long_description" >
-        </textarea>
-                    </div>
-
-                    <div class="form-control mt-4">
-                        <button class="btn btn-primary w-full">Submit</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
 
     <x-nav></x-nav>
 
